a5408f31a5ccc309e870d55d144d45b25b08e81c
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2025-02-13 20:29:16 +0000

    Added category fitering functionality

--- products/views.py
+++ products/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
-# Create your views here.
 
 def all_products(request):
     """A view to show all products, including sorting and searching queries """
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -19,11 +24,11 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
 
     context = {
         'products': products,
-        'search_term': query, 
+        'search_term': query,
+        'current_categorie': categories,
     }
 
     return render(request, 'products/products.html', context)
